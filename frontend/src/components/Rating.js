cd17ec4c1092ffa680dc23594c4ee64f85bab096
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-02-14 02:47:25 -0500

    built login page. tracking down db issues

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 
 const rating = ({ value, text, color }) => {
   return (
-    <div class='rating'>
+    <div className='rating'>
       <span>
         <i
           style={{ color: color }}
-          class={
+          className={
             value >= 1
               ? 'fas fa-star'
               : value >= 0.5
@@ -19,7 +19,7 @@ const rating = ({ value, text, color }) => {
       <span>
         <i
           style={{ color: color }}
-          class={
+          className={
             value >= 2
               ? 'fas fa-star'
               : value >= 1.5
@@ -31,7 +31,7 @@ const rating = ({ value, text, color }) => {
       <span>
         <i
           style={{ color: color }}
-          class={
+          className={
             value >= 3
               ? 'fas fa-star'
               : value >= 2.5
@@ -43,7 +43,7 @@ const rating = ({ value, text, color }) => {
       <span>
         <i
           style={{ color: color }}
-          class={
+          className={
             value >= 4
               ? 'fas fa-star'
               : value >= 3.5
@@ -55,7 +55,7 @@ const rating = ({ value, text, color }) => {
       <span>
         <i
           style={{ color: color }}
-          class={
+          className={
             value >= 5
               ? 'fas fa-star'
               : value >= 4.5
