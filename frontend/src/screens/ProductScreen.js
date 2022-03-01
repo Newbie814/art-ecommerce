16247f7a64938a00565118aa91bc5b1866e4643b
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-03-01 15:20:45 -0500

    added media query for small pics on phone homepage

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -85,7 +85,12 @@ const ProductScreen = ({ history, match }) => {
           <Meta title={product.name} />
           <Row>
             <Col md={6}>
-              <Image src={product.image} alt={product.name} fluid />
+              <Image
+                src={product.image}
+                alt={product.name}
+                style={{ minHeight: '150px' }}
+                fluid
+              />
             </Col>
             <Col md={3}>
               <ListGroup variant='flush'>
